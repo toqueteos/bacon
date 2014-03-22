3be386c98623555cacc933554a66d3996fa78597
Carlos Cobo <toqueteos@gmail.com>
2014-03-22 01:41:37 +0100

    Remove unnecessary methods.

--- Assets/EndlessFloor.cs
+++ Assets/EndlessFloor.cs
@@ -6,16 +6,6 @@ public class EndlessFloor : MonoBehaviour
 
 	public int offset = 0;
 
-	void Start()
-	{
-	
-	}
-	
-	void Update()
-	{
-	
-	}
-
 	void OnTriggerEnter(Collider other) {
 		Vector3 pos = other.transform.position;
 		pos.z -= offset;
