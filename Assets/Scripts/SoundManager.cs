88e94a8d30fdb9d162073d4c6799731f201b0015
Carlos Cobo <toqueteos@gmail.com>
2014-03-23 03:51:55 +0100

    SoundManager, BGM prefabs and more sounds.

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+
+	[Range(0.00f, 1.00f)]
+	public float volume = 1;
+	public AudioSource startTrack;
+	public bool playOnStart = false;
+	public AudioSource[] bgms;
+	bool play = false;
+	AudioSource current = null;
+	float currentVolume;
+
+	void Start()
+	{
+		bgms = GetComponentsInChildren<AudioSource>();
+		currentVolume = volume;
+
+		if (startTrack != null) {
+			current = startTrack;
+			current.Play();
+			play = true;
+		}
+	}
+	
+	void Update()
+	{
+		if (current != null && volume != currentVolume)
+		{
+			currentVolume = volume;
+			current.volume = volume;
+		}
+
+		Toggle(0, KeyCode.Alpha1);
+		Toggle(1, KeyCode.Alpha2);
+	}
+
+	void Toggle(int index, KeyCode key) {
+		if (Input.GetKeyDown(key)) {
+			if (!play) {
+				if (current != null) {
+					current.Stop();
+				}
+				current = bgms[index];
+				current.Play();
+			} else {
+				current.Stop();
+			}
+		}
+	}
+}
