0b80070cd2e4dacd35372eac4695d9d3dd00ad8d
Sithdown <afdezmnez@gmail.com>
2014-03-23 14:31:55 +0100

    New obstacle generator
    WAKAME! WAKAME!

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+
+	// hostile 0-4
+	// neutral 5-8
+	// powerups 9-INFINITY
+	public GameObject[] props;
+
+	public Transform player;
+
+	public Vector2 timeOffset;
+	private float counter;
+
+	void Start()
+	{
+		counter = Random.Range(timeOffset.x,timeOffset.y);
+	}
+
+	void Update()
+	{
+		counter -= Time.deltaTime;
+		if(counter<=0){
+
+			// Random prefab
+			int i = Random.Range(0, props.Length);
+			GameObject go = Instantiate(props[i]) as GameObject;
+			go.transform.parent = transform;
+			go.transform.position = player.position;
+
+			counter = Random.Range(timeOffset.x,timeOffset.y);
+		}
+	}
+}
