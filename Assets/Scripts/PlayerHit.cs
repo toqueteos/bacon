c67789f2aa34423354ba048879bb0907b7d37774
Carlos Cobo <toqueteos@gmail.com>
2014-03-23 21:21:17 +0100

    Play sound on player collision (shitty version).

---
+++ Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHit : MonoBehaviour {
+
+	public AudioSource boo;
+
+	void OnCollisionEnter(Collision collision) {
+		GameManager.instance.AddHunger(-0.1f);
+		Debug.Log(string.Format("Hit {0}", collision.transform.name));
+		boo.Play();
+	}
+}
