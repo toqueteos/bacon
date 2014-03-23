5e1e0de77d39ab4f7c0879f06e99f9a431f0a969
Carlos Cobo <toqueteos@gmail.com>
2014-03-23 13:35:34 +0100

    Move camera outside player.

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+	public Transform player;
+
+	void Update()
+	{
+		transform.position = player.position;
+	}
+}
