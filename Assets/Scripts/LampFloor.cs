089b09c40391fb56130c69ca73592b4825eebed5
Carlos Cobo <toqueteos@gmail.com>
2014-03-23 17:27:11 +0100

    Light spawning working.

---
+++ Assets/Scripts/LampFloor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampFloor : MonoBehaviour
+{
+
+	public Vector3 spawn;
+	static bool swapSide = false;
+
+	void Start()
+	{
+		if (swapSide) {
+			spawn.x = -spawn.x;
+		}
+		
+		swapSide = !swapSide;
+
+		transform.position += spawn;
+	}
+}
