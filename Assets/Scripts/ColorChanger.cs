4c0dca5d5c8198353bf46f84f7cc9f44db987987
Carlos Cobo <toqueteos@gmail.com>
2014-03-23 20:50:09 +0100

    Refactor GameManager.

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -6,20 +6,50 @@ public class ColorChanger : MonoBehaviour
 
 	public int change = 30;
 	SpriteRenderer sr;
+	Animator anim;
 	int counter = 0;
 
 	void Start()
 	{
 		sr = GetComponent<SpriteRenderer>();
+		if (sr == null)
+		{
+			Debug.LogError("SpriteRenderer component is missing.");
+		}
+
+		anim = GetComponent<Animator>();
+		if (anim == null)
+		{
+			Debug.LogError("Animator component is missing.");
+		}
 	}
 	
 	void Update()
 	{
 		counter = (counter + 1) % change;
 
-		if (counter == 0) {
+		if (counter == 0)
+		{
 			Color c = new Color(Random.value, Random.value, Random.value);
 			sr.color = c;
 		}
+	}
+
+	void OnEnable()
+	{
+		anim = GetComponent<Animator>();
+		if (anim == null)
+		{
+			Debug.LogError("Animator component is missing.");
+		}
+	}
+
+	void OnDisable()
+	{
+		anim.speed = 1f;
+	}
+
+	public void SetAnimationSpeed(float s) {
+		anim.speed = s;
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,9 @@ public class GameManager : MonoBehaviour
 
 	public static GameManager instance;
 	public UIProgressBar hunger;
+	public float fillRate = 0.05f;
 	public float hurryUpStart = 0.75f;
+	public float hurryUpAnimationSpeed = 3.5f;
 	bool hurryUp = false;
 	public ColorChanger flashyBackground;
 
@@ -22,13 +24,20 @@ public class GameManager : MonoBehaviour
 			Debug.LogError("ColorChanger component is missing.");
 		}
 	}
+
+	void Update() {
+		AddHunger(fillRate * Time.deltaTime);
+	}
 	
 	public void AddHunger(float h) {
-		hunger.value = h;
+		hunger.value = Mathf.Clamp(hunger.value + h, 0f, 1f);
 
 		bool hurry = hunger.value >= hurryUpStart;
 
-		hurryUp = hurry;
 		flashyBackground.enabled = hurry;
+
+		if (hurry) {
+			flashyBackground.SetAnimationSpeed(hurryUpAnimationSpeed);
+		}
 	}
 }
