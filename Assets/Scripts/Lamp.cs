a852e5ceb61f44cb1c6548d1f6f83426c30f57a8
Carlos Cobo <toqueteos@gmail.com>
2014-03-23 17:14:19 +0100

    Lamp Tokio.

--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class Lamp : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Vector3 spawn;
+	public bool flicker = true;
+	public Vector2 flickerRange;
+	static bool swapSide = false;
+
+	void Start ()
+	{
+		if (swapSide) {
+			spawn.x = -spawn.x;
+		}
+
+		swapSide = !swapSide;
+
+		transform.position += spawn;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+
+		if (Random.Range(flickerRange.x, flickerRange.y) == 0)
+		{
+			StartCoroutine("Flicker");
+		}
+	}
+
+	IEnumerator Flicker() {
+		float i = light.intensity;
+		yield return new WaitForSeconds(0.5f);
+		light.intensity = 0.2f;
+		yield return new WaitForSeconds(0.5f);
+		light.intensity = 0.5f;
+		yield return new WaitForSeconds(0.5f);
+		light.intensity = 0.2f;
+		yield return new WaitForSeconds(0.5f);
+		light.intensity = i;
 	}
 }
